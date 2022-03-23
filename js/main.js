955362de373ab220893818bebfd35fc75dd8fcee
aboongm <aboongm@yahoo.com>
2022-03-23 13:30:26 +0530

    Add styles and dynamic js

--- js/main.js
+++ js/main.js
@@ -4,6 +4,12 @@ const book = document.querySelector('#book');
 const author = document.querySelector('#author');
 const KEY = 'BOOKS_LIST';
 
+const list = document.querySelector('.list');
+const add = document.querySelector('.add');
+const contact = document.querySelector('.contact');
+const date = document.querySelector('#date');
+const contactInfo = document.querySelector('.contact-info');
+
 let BooksObject = [];
 
 class Books {
@@ -25,9 +31,12 @@ class Books {
   }
 
   remove(element) {
-    const { id } = element.dataset;
+    const id = element.dataset.id;
     element.parentElement.remove();
-    this.BooksObject.splice(this.BooksObject.findIndex((item) => item.id === parseInt(id, 10)), 1);
+    this.BooksObject.splice(
+      this.BooksObject.findIndex((item) => item.id === parseInt(id, 10)),
+      1
+    );
     localStorage.setItem('BOOKS_LIST', JSON.stringify(this.BooksObject));
   }
 }
@@ -37,7 +46,8 @@ function loadContent() {
   BooksObject.forEach((obj) => {
     booksList.innerHTML += `<div class="book-container">
                     <div class="book">
-                      <h4 class="text-1">"${obj.title}" by ${obj.author}</h4>
+                      <h4 class="text-1">"${obj.title}"</h4>
+                      <h3 class="text-1">by ${obj.author}</h3>
                     </div>
                     <button type="button" onclick="removeBook(this)" class="btn" data-id="${obj.id}">Remove</button>
                 </div>`;
@@ -59,13 +69,45 @@ function addBook() {
   loadContent();
 }
 
-/* eslint-disable */
-
 function removeBook(element) {
   books.remove(element);
 }
-/* eslint-enable */
 
 addBtn.addEventListener('click', addBook);
+list.addEventListener('click', (e) => {
+  booksList.parentElement.classList.remove('hide');
+  author.parentElement.parentElement.classList.add('hide');
+  contactInfo.classList.add('hide');
+});
+add.addEventListener('click', () => {
+  author.parentElement.parentElement.classList.remove('hide');
+  booksList.parentElement.classList.add('hide');
+  contactInfo.classList.add('hide');
+});
+
+contact.addEventListener('click', () => {
+  author.parentElement.parentElement.classList.add('hide');
+  booksList.parentElement.classList.add('hide');
+  contactInfo.classList.remove('hide');
+});
+
+const getDateTime = () => {
+  const date = new Date();
+  let options = {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: 'true',
+  };
+  return date.toLocaleString('en-US', options);
+};
+date.innerHTML = `
+<h4>
+ ${getDateTime()}
+</h4>
+`;
 
 document.addEventListener('DOMContentLoaded', checkLocalStorage);
